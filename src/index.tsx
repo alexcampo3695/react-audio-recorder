fb7c4ddab9a5e94388f686cce483dfcd8c869aca
Alex Campo <alex.campo@antidote-ai.com>
2024-05-06 14:15:33 -0400

    commit without open AI KEY 😅

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,3 @@
 export { default as AudioRecorder } from "./components/AudioRecordingComponent";
 export { default as useAudioRecorder } from "./hooks/useAudioRecorder";
 
-require("dotenv").config();
-
-console.log(process.env.OPENAI_API_KEY);
