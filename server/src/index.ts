254491d4032ec0d3defab61fc23ae70083171a12
Alex Campo <alex.campo@antidote-ai.com>
2024-05-07 16:16:34 -0400

    env variable for mongo

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
+import { config } from 'dotenv';
+config();
+
 import express, {Request, Response} from 'express';
 import mongoose from 'mongoose';
 import Deck from "./models/Deck";
+
 
 const app = express();
 
@@ -17,7 +21,7 @@ app.post('/decks', async (req : Request, res: Response) => {
 })
 
 mongoose.connect(
-    'mongodb+srv://antidoteTranscribe:hQmPiLtvP5VNBdtC@cluster0.f7fkxds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    process.env.MONGO_URL!
 ).then(() => {
     console.log(`Connected to database on port: ${PORT}`);
     app.listen(PORT);
