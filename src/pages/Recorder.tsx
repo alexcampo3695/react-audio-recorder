afc05d588fc6076d6113a6e77231a9a829c4f42e
Alex Campo <alex.campo@antidote-ai.com>
2024-05-09 18:31:57 -0400

    further audio recorder enhancements

--- src/pages/Recorder.tsx
+++ src/pages/Recorder.tsx
@@ -44,13 +44,13 @@ const RecorderPage: React.FC<RecorderPageProps> = ({
       children={
         <div>
           {/* <UserProfile FirstName="John" LastName="Doe" DateOfBirth="01/01/1990" /> */}
-          {/* <AudioRecorder
+          <AudioRecorder
             onRecordingComplete={handleRecordingComplete}
             onRecordingStateChange={handleRecordingStateChange}
             isRecording={isRecording} 
           />
           <AudioUploader onFileUpload={onFileUpload} />
-          {patientData && <PatientDetails patientData={patientData} />} */}
+          {patientData && <PatientDetails patientData={patientData} />}
           {/* {isRecording && (
             <div className={`antidote-recorder-button ${isRecording ? 'is-recording' : ''}`}>
               <img className="light-image recorder-emblem" src={antidoteEmblem} alt="" />
@@ -64,7 +64,7 @@ const RecorderPage: React.FC<RecorderPageProps> = ({
           </div>
           <div className="antidote-controls-container">
             <div className="deleted">
-            <i><svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></i>
+            <i><svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></i>
             </div>
             <div className="pause">
               <span>Resume</span>
