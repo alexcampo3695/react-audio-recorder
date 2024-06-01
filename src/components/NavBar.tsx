a94a3b931aea11f2874787ce3ae0ad3ce26cb10b
Alex Campo <alex.campo@antidote-ai.com>
2024-06-01 17:48:05 -0400

    tab switch on recorder style fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,14 @@
-import React, { useState, useEffect, ReactElement, Suspense } from "react";
-import { Props } from "./interfaces";
-import { format } from "path";
+import React, { useState, useEffect } from "react";
 import antidoteEmblem from "../styles/assets/Antidote_Emblem.svg";
-import { useUser } from '../context/UserContext'
+import { useUser } from '../context/UserContext';
 import FakeAvatar, { AvatarSize } from "../elements/FakeAvatar";
+import { useNavigate } from "react-router-dom";
 
-
-const NavBar = ({}) => {
+const NavBar = () => {
     const { user } = useUser();
-    const [menuTab, setActiveTab] = useState('home')
-    const [settingMenu, setSettingMenu] = useState(false)
-    const [data, setData] = useState<any>({
+    const navigate = useNavigate();
+    const [menuTab, setActiveTab] = useState('home');
+    const [data, setData] = useState({
         firstName: '',
         lastName: '',
         gender: '',
@@ -22,12 +20,14 @@ const NavBar = ({}) => {
         stateLicenseNumber: '',
         deaNumber: ''
     });
+    const [settingMenu, setSettingMenu] = useState(false);
 
-    const handleTabClick = (tab:string, event: React.MouseEvent) => {
+    const handleTabClick = (tab: string, path: string, event: React.MouseEvent) => {
         event.preventDefault();
         setActiveTab(tab);
+        navigate(path);
     }
-    
+
     useEffect(() => {
         const fetchUserDetails = async () => {
             if (user) {
@@ -57,86 +57,61 @@ const NavBar = ({}) => {
                 </a>
             </div>
             <div className="sidebar-inner">
-                <div 
-                    className="naver" 
-                    // style="margin-top: 150px;"
-                > 
-                </div>
-
+                <div className="naver"></div>
                 <ul className="icon-menu">
                     <li>
-                        <a 
-                            href="/" 
-                            id="home-sidebar-menu" 
-                            data-content="Dashboards" 
-                            onClick={(e) => handleTabClick('home', e)}
+                        <a
+                            href="/home"
+                            id="home-sidebar-menu"
+                            data-content="Dashboards"
+                            onClick={(e) => handleTabClick('home', '/home', e)}
                             className={menuTab === 'home' ? "is-selected is-active" : ""}
                         >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-activity sidebar-svg"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-activity sidebar-svg"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline></svg>
                         </a>
                     </li>
                     <li>
-                        <a 
-                            href="/table" 
-                            id="layouts-sidebar-menu" 
+                        <a
+                            href="/table"
+                            id="layouts-sidebar-menu"
                             data-content="Layouts"
-                            onClick={(e) => handleTabClick('table', e)}
-                            className={menuTab === 'table' ? "is-selected is-active" : ""} 
+                            onClick={(e) => handleTabClick('table', '/table', e)}
+                            className={menuTab === 'table' ? "is-selected is-active" : ""}
                         >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-grid sidebar-svg"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-grid sidebar-svg"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
                         </a>
                     </li>
-                    {/* <li>
-                        <a href="elements-hub.html" id="elements-sidebar-menu" data-content="Elements">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-box sidebar-svg"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="components-hub.html" id="components-sidebar-menu" data-content="Components">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-cpu sidebar-svg"><rect x="4" y="4" width="16" height="16" rx="2" ry="2"></rect><rect x="9" y="9" width="6" height="6"></rect><line x1="9" y1="1" x2="9" y2="4"></line><line x1="15" y1="1" x2="15" y2="4"></line><line x1="9" y1="20" x2="9" y2="23"></line><line x1="15" y1="20" x2="15" y2="23"></line><line x1="20" y1="9" x2="23" y2="9"></line><line x1="20" y1="14" x2="23" y2="14"></line><line x1="1" y1="9" x2="4" y2="9"></line><line x1="1" y1="14" x2="4" y2="14"></line></svg>
-                        </a>
-                    </li>
-                    <li id="messages-menu">
-                        <a href="admin-messaging-chat.html" id="open-messages" data-content="Messaging">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-message-circle sidebar-svg"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
-                        </a>
-                    </li> */}
                 </ul>
 
                 <ul className="bottom-menu">
-                    {/* <li className="right-panel-trigger" data-panel="search-panel">
-                        <a href="javascript:void(0);" id="open-search" data-content="Search"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search sidebar-svg"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg></a>
-                        <a href="javascript:void(0);" id="close-search" className="is-hidden is-inactive"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x sidebar-svg"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></a>
-                    </li> */}
                     <li>
                         <a href="/profile_settings" id="open-settings" data-content="Settings">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-settings sidebar-svg"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
                         </a>
                     </li>
                     <li id="user-menu">
-                        <div 
+                        <div
                             id="profile-menu"
-                            onClick = {() => setSettingMenu(!settingMenu)} 
-                            className= {`dropdown profile-dropdown dropdown-trigger is-spaced is-up ${settingMenu ? 'is-active' : ''}`}
+                            onClick={() => setSettingMenu(!settingMenu)}
+                            className={`dropdown profile-dropdown dropdown-trigger is-spaced is-up ${settingMenu ? 'is-active' : ''}`}
                         >
-                            <FakeAvatar 
-                                FirstName={data.firstName !== '' ? data.firstName : 'U'} 
-                                LastName={data.lastName !== '' ? data.lastName : 'K'} 
-                                Size={AvatarSize.Medium} 
+                            <FakeAvatar
+                                FirstName={data.firstName !== '' ? data.firstName : 'U'}
+                                LastName={data.lastName !== '' ? data.lastName : 'K'}
+                                Size={AvatarSize.Medium}
                             />
                             <span className="status-indicator"></span>
                             <div className="dropdown-menu" role="menu">
                                 <div className="dropdown-content">
                                     <div className="dropdown-head">
-                                    <FakeAvatar 
-                                        FirstName={data.firstName !== '' ? data.firstName : 'U'} 
-                                        LastName={data.lastName !== '' ? data.lastName : 'K'} 
-                                        Size={AvatarSize.Large} 
-                                    />
+                                        <FakeAvatar
+                                            FirstName={data.firstName !== '' ? data.firstName : 'U'}
+                                            LastName={data.lastName !== '' ? data.lastName : 'K'}
+                                            Size={AvatarSize.Large}
+                                        />
                                         <div className="meta">
                                             <span>
                                                 {data.firstName !== '' ? data.firstName : 'Uknown'}{' '}
-                                                
                                                 {data.firstName !== '' ? data.lastName : 'User'}
                                             </span>
                                             <span>
@@ -153,19 +128,9 @@ const NavBar = ({}) => {
                                             <span>View your profile</span>
                                         </div>
                                     </a>
-                                    {/* <hr className="dropdown-divider"></hr>
-                                    <a href="#" className="dropdown-item is-media">
-                                        <div className="icon">
-                                            <i className="lnil lnil-cog"></i>
-                                        </div>
-                                        <div className="meta">
-                                            <span>Settings</span>
-                                            <span>Account settings</span>
-                                        </div>
-                                    </a> */}
                                     <hr className="dropdown-divider"></hr>
                                     <div className="dropdown-item is-button">
-                                        <button 
+                                        <button
                                             className="button h-button is-primary is-raised is-fullwidth logout-button"
                                             onClick={() => {
                                                 localStorage.removeItem('user');
@@ -185,8 +150,7 @@ const NavBar = ({}) => {
                 </ul>
             </div>
         </div>
-    )
+    );
 };
-
 
 export default NavBar;
--- src/pages/Recorder.tsx
+++ src/pages/Recorder.tsx
@@ -6,8 +6,6 @@ import PatientDetails from "./PatientDetails";
 import AppWrapper from "./AppWrapper";
 import antidoteEmblem from "../styles/assets/Antidote_Emblem.svg";
 import "../styles/recorder-button.scss";
-import UserProfile from "../components/UserProfile";
-import { transcribeAudio } from "../helpers/transcribe";
 import AudioUploader from "../components/AudioUploader";
 import { useUser } from "../context/UserContext";
 
@@ -51,7 +49,6 @@ const RecorderPage: React.FC<RecorderPageProps> = ({
   async function handleRecordingComplete(blob: Blob) {
     const formData = new FormData();
     formData.append("recording", blob, "recording.webm");
-    console.log("Patient Data to be sent:", JSON.stringify(patientData));
     
     if (patientData) {
       formData.append("patientData", JSON.stringify(patientData));  // Correct the key here
@@ -72,7 +69,6 @@ const RecorderPage: React.FC<RecorderPageProps> = ({
       });
       if (response.ok) {
         const data = await response.json();
-        console.log("Recording uploaded successfully", data);
         onRecordingComplete(blob);
         setIsRecording(false);
         navigate('/table');
@@ -84,10 +80,12 @@ const RecorderPage: React.FC<RecorderPageProps> = ({
     }
   }
   
-  const [activeTab, setActiveTab] = useState('create');
+  const [activeTab, setActiveTab] = useState('record');
     
-  const handleTabClick = (tab: string) => {
+  const handleTabClick = (tab: string, event:React.MouseEvent) => {
+    event.preventDefault()
     setActiveTab(tab);
+    console.log(activeTab)
   }
 
   const memoizedAudioRecorder = useMemo(() => (
@@ -102,56 +100,44 @@ const RecorderPage: React.FC<RecorderPageProps> = ({
     <AppWrapper
       title="Recorder"
       children={
-        <div>
-          <div 
-            // className="tabs-inner switch"
-          >
-            <div 
-              // className="tabs"
-            >
-              <ul>
-                <li 
-                  data-tab="active-items-tab" 
-                  className={activeTab === 'create' ? "is-active" : ''}
-                  onClick={() => handleTabClick('create')}
-                >
-                  <a>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-upload-cloud">
-                      <polyline points="16 16 12 12 8 16"></polyline>
-                      <line x1="12" y1="12" x2="12" y2="21"></line>
-                      <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
-                      <polyline points="16 16 12 12 8 16"></polyline>
-                    </svg>
-                    <span>Create Patient</span>
-                  </a>
-                </li>
-                <li 
-                  data-tab="inactive-items-tab"
-                  className={activeTab === 'existing' ? "is-active" : ''}
-                  onClick={() => handleTabClick('existing')}
-                >
-                  <a>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-users">
-                      <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                      <circle cx="9" cy="7" r="4"></circle>
-                      <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-                      <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-                    </svg>
-                    <span>Existing Patients</span>
-                  </a>
-                </li>
-                <li className="tab-naver"></li>
-              </ul>
+        <>
+          <div className="tabs-wrapper is-slider is-squared" style={{display: 'flex', justifyContent: 'center'}}>
+            <div className="tabs-inner">
+                <div className="tabs">
+                  <ul>
+                    <li 
+                      data-tab="team-tab1" 
+                      className={activeTab === 'record' ? 'is-active' : ''}
+                      onClick={(e) => handleTabClick('record', e)}
+                    >
+                      <a>
+                        <span>Recorder</span>
+                      </a>
+                    </li>
+                    <li 
+                      data-tab="projects-tab1"
+                      className={activeTab === 'upload' ? 'is-active': ''}
+                      onClick={(e) => handleTabClick('upload', e)}
+                    >
+                      <a>
+                        <span>Upload</span>
+                      </a>
+                    </li>
+                    <li className="tab-naver"></li>
+                  </ul>
+                </div>
             </div>
           </div>
-          {activeTab === 'create' ? (
-            memoizedAudioRecorder
-          ) : (
-            <AudioUploader onFileUpload={onFileUpload} />
-            
-          )}
-          {/* { patientData && <PatientDetails patientData={patientData} />} */}
-        </div>
+          <div>
+            {activeTab === 'record' ? (
+              memoizedAudioRecorder
+            ) : (
+              <AudioUploader onFileUpload={onFileUpload} />
+              
+            )}
+            {/* { patientData && <PatientDetails patientData={patientData} />} */}
+          </div>
+        </>
       }
     />
   );
