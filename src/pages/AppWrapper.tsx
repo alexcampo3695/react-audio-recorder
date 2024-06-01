41b5b296686a9887b1a73eb6c70bf9d66c890802
Alex Campo <alex.campo@antidote-ai.com>
2024-06-01 17:52:53 -0400

    app wrapper clean up🛠️

--- src/pages/AppWrapper.tsx
+++ src/pages/AppWrapper.tsx
@@ -71,7 +71,7 @@ const AppWrapper = ({ children, title}: AppWrapperProps) => {
                                 </label>
                             </div>
 
-                            <a className="toolbar-link right-panel-trigger" data-panel="languages-panel">
+                            {/* <a className="toolbar-link right-panel-trigger" data-panel="languages-panel">
                                 <img src="assets/img/icons/flags/united-states-of-america.svg" alt=""></img>
                             </a>
 
@@ -94,11 +94,11 @@ const AppWrapper = ({ children, title}: AppWrapperProps) => {
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </div> */}
 
-                            <a className="toolbar-link right-panel-trigger" data-panel="activity-panel">
+                            {/* <a className="toolbar-link right-panel-trigger" data-panel="activity-panel">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-grid"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
-                            </a>
+                            </a> */}
                         </div>
                     </div>
 
