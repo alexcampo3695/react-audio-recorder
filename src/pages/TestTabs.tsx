1fe6f2f6238b71663dcededc791b224a93ab5b72
Alex Campo <alex.campo@antidote-ai.com>
2024-06-16 17:01:10 -0400

    navbar style fixes and routing!

--- src/pages/TestTabs.tsx
+++ src/pages/TestTabs.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../styles/mobile-nav.scss';
 
 const TabsTest: React.FunctionComponent = () => (
   <div className="navbar-ios">
     <li className="list-item-ios">
-      <i className="fas fa-home"></i>
-      <span className="list-item-name-ios">Home</span>
+      <Link to="/home">
+        <i className="fas fa-home"></i>
+        {/* <span className="list-item-name-ios">Home</span> */}
+      </Link>
     </li>
     <li className="list-item-ios">
-      <i className="fas fa-list"></i>
-      <span className="list-item-name-ios">Recordings</span>
+      <Link to="/table">
+        <i className="fas fa-list"></i>
+        {/* <span className="list-item-name-ios">Recordings</span> */}
+      </Link>
     </li>
     <li className="list-item-ios">
-      <i className="fas fa-user"></i>
-      <span className="list-item-name-ios">Settings</span>
+      <Link to="/profile_settings">
+        <i className="fas fa-user"></i>
+        {/* <span className="list-item-name-ios">Settings</span> */}
+      </Link>
     </li>
   </div>
 );
