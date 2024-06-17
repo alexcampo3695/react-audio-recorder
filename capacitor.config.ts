012ed0c392c774b71cb3fe7510ee1f31f597f592
Alex Campo <alex.campo@antidote-ai.com>
2024-06-16 23:31:48 -0400

    ios deployment

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.transcribe.app',
-  appName: 'transcribe',
+  appId: 'com.carevoice.app',
+  appName: 'CareVoice',
   webDir: 'dist',
   server: {
     url: 'http://localhost:4173/',
