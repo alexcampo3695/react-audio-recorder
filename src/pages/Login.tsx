6ef444c0aab7ef6deeecfddae3fa545398fac195
Alex Campo <alex.campo@antidote-ai.com>
2024-06-28 15:27:12 -0400

    tls encrypt

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -112,7 +112,7 @@ const Login = () => {
                             <div className="form-text">
                                 <h2>Sign In</h2>
                                 <p>Welcome back to your account.</p>
-                                <p>V3</p>
+                                <p>V4</p>
                             </div>
                             {error && <div className="notification is-danger">{error}</div>}
                             <form id="login-form" className="login-wrapper" onSubmit={handleLogin}>
--- src/version.js
+++ src/version.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-module.exports = { VERSION: '20240627172907' };
-=======
-module.exports = { VERSION: '20240626204120' };
->>>>>>> 9012301133c1efa5e3e08e5b483030ef462be5fc
+module.exports = { VERSION: '20240628122706' };
